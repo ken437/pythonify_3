4b887dcd4b4f2376aee819bbc39f80dd2991338b
GitHub Action <action@github.com>
2021-10-15 17:29:00 +0000

    Template cleanup

--- src/main/kotlin/com/github/ken437/pythonify3/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/ken437/pythonify3/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.ken437.pythonify3.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.ken437.pythonify3.MyBundle
 
 class MyApplicationService {
 
